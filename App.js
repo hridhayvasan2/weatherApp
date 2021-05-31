86e1fc7bec640f75ead8cf0b1f9d6daa7bd038b7
hridhayvasan2 <81846437+hridhayvasan2@users.noreply.github.com>
2021-05-31 19:27:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+import BuzzerScreen from './screens/BuzzerScreen'
+import { createAppContainer, createSwitchNavigator} from 'react-navigation'; 
+
+export default class WeatherScreen extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      weather:''
+    }
+  }
+
+  getWeather = async()=>{
+    var url = 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139';
+    return fetch(url)
+    .then(response=>response.json())
+    .then(responseJson=>{
+      this.setState({
+        weather:responseJson,
+      })
+    })
+    .catch(error=>{
+      console.error(error)
+    })
+  }
+
+  componentDidMount=()=>{
+    this.getWeather();
+  }
+
+  render() {
+    if(this.state.weather===''){
+      return(
+        <View style={styles.container}>
+        <Text>
+        LOADING...
+        </Text>
+        </View>
+      )
+    }
+    else{
+      return (
+        <View style={styles.container}>
+          <View style={styles.subContainer}>
+            <Text>Weather Forecast</Text>
+            <Image
+            style={styles.cloudImage}
+            source={require('./clouds.png')}
+            />
+            <View style={styles.textContainer}>
+            <Text styles={{fontSize:18}}>
+            {this.state.weather.main.temp}&deg;C
+            
+            </Text>
+            <Text style={{fontSize:20,margin:10}}>
+            Humidity:{this.state.weather.main.humidity}
+            </Text>
+            <Text style={{fontSize:20}}>
+            {this.state.weather.weather[0].description}
+            </Text>
+            </View>
+
+          </View>
+          
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({ container: { flex:1 }, subContainer : { flex: 1, borderWidth: 1, alignItems: 'center' }, title:{ marginTop: 50, fontSize: 30, fontWeight: '550' }, cloudImage :{ width: 200, height: 200, marginTop: 30 }, textContainer : { flex: 1, alignItems: 'center', flexDirection:'row', marginTop:-150 } });
